83c8af1336fce697cb45f989b70c9cfd5fa19d9d
Kenny Marreira <kennymarreira@hotmail.com>
2023-03-13 11:30:11 -0300

    Function 1 Implementacao da Interface

---
+++ ex 093 Parte 257 Exercicio001 FunctionProgramacaoFuncional/src/util/UpperCaseName.java
@@ -0,0 +1,16 @@
+package util;
+
+import java.util.function.Function;
+
+import entities.Product;
+
+public class UpperCaseName implements Function<Product, String> {
+
+	@Override
+	public String apply(Product p) {
+		return p.getName().toUpperCase();
+	}
+	
+	
+	
+}

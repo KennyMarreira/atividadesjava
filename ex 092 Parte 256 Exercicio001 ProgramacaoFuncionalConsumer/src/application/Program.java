e6ffde946cde1a5b3a4bdcb04ca6944cf416d69f
Kenny Marreira <kennymarreira@hotmail.com>
2023-03-13 11:05:02 -0300

    Consumer 4 expressao lambda declarada

--- ex 092 Parte 256 Exercicio001 ProgramacaoFuncionalConsumer/src/application/Program.java
+++ ex 092 Parte 256 Exercicio001 ProgramacaoFuncionalConsumer/src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -18,7 +19,13 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::nonStaticPriceUpdate);
+		double factor = 1.1;
+		
+		Consumer<Product> cons = p -> {
+			p.setPrice(p.getPrice() * factor);
+		};
+		
+		list.forEach(cons);
 		
 		list.forEach(System.out::println);
 		

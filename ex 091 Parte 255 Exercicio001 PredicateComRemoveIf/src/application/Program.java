59b2f1d6181e321e3578d94725b9b9d9246465c1
Kenny Marreira <kennymarreira@hotmail.com>
2023-03-13 10:28:23 -0300

    Predicate RemoveIf 5 Expressao Lambda inline

--- ex 091 Parte 255 Exercicio001 PredicateComRemoveIf/src/application/Program.java
+++ ex 091 Parte 255 Exercicio001 PredicateComRemoveIf/src/application/Program.java
@@ -3,7 +3,6 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Predicate;
 
 import entities.Product;
 
@@ -21,9 +20,7 @@ public class Program {
 		
 		double min = 100.0;
 		
-		Predicate<Product> pred = p -> p.getPrice() >= min;
-		
-		list.removeIf(pred);
+		list.removeIf(p -> p.getPrice() >= min);
 		
 		for (Product p : list) {
 			System.out.println(p);

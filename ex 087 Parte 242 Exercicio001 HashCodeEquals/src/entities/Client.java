f9d559d85b7f0cf56dffe2b06a69427a3bd298bd
Kenny Marreira <kennymarreira@hotmail.com>
2023-03-12 21:27:24 -0300

    HashCode, Equals e Set

---
+++ ex 087 Parte 242 Exercicio001 HashCodeEquals/src/entities/Client.java
@@ -0,0 +1,51 @@
+package entities;
+
+import java.util.Objects;
+
+public class Client {
+	
+	private String nome;
+	private String email;
+	
+	public Client(String nome, String email) {
+		super();
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(email, other.email) && Objects.equals(nome, other.nome);
+	}
+	
+	
+
+}
